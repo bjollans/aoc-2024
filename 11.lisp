1bb8facbd8067906a473618875d5d824004092bb
Bernard Jollans <berjollans@gmail.com>
2024-12-12 13:06:56 +0100

    Scrapping cache idea

--- 11.lisp
+++ 11.lisp
@@ -32,6 +32,24 @@
 ;; UTILITY
 ;; *******
 
+(defun count-unique (lst)
+  (if (equal '() lst)
+      0
+      (+
+       (if (member (car lst) (cdr lst) :test #'equal) 0 1)
+       (count-unique (cdr lst)))))
+
+(defun unique (lst)
+  (if (equal '() lst)
+      '()
+      (append
+        (if (member (car lst) (cdr lst) :test #'equal) '() (list (first lst)))
+        (unique (cdr lst)))))
+
+(defun count-val (val lst)
+  (count val lst :test #'equal))
+
+
 (defun split (input)
   (split-sequence:split-sequence #\Space input))
 
@@ -61,70 +79,22 @@
 (assert (equal (list "2024") (transform-stone "1")))
 (assert (equal (list "20" "24") (transform-stone "2024")))
 
+(defun tree-blink (stones-list times &optional (depth 0))
+  (if (= depth times)
+      (length stones-list)
+      (apply #'+ (mapcar (lambda (stone)
+                (* (count-val stone stones-list) (tree-blink (transform-stone stone) times (1+ depth))))
+          (unique stones-list)))))
 
-(defun transform-stones (stones)
-  (join (mapcan #'transform-stone (split stones))))
+(assert (equal 3 (tree-blink (split test-input) 1)))
+(assert (equal 13 (tree-blink (split test-input) 5)))
+(assert (equal 22 (tree-blink (split test-input) 6)))
+(assert (equal 55312 (tree-blink (split test-input) 25)))
 
-(assert (equal "253000 1 7" (transform-stones test-input)))
+; (defun do-part-1 (input)
+;   (length (tree-blink (split input) 25)))
 
-(defun blink (stones times)
-  (with-print (format nil "blinking stones. Times: ~d; Length: ~d" times (length (split stones)))
-              (if (= 0 times)
-                  stones
-                  (blink (transform-stones stones) (1- times)))))
+; (assert (= 55312 (do-part-1 test-input)))
+; (assert (= 55312 (length (tree-blink (split test-input) 25))))
 
-
-(defparameter stone-cache (make-hash-table))
-
-(defun get-from-cache (stone)
-  (let ((cache-key (parse-integer stone)))
-    (gethash cache-key stone-cache)))
-
-(defun add-to-cache (stone result)
-  (let ((cache-key (parse-integer stone)))
-    (setf (gethash cache-key stone-cache) result)))
-
-(assert (not (get-from-cache "0")))
-(add-to-cache "0" 34)
-(assert (= 34 (get-from-cache "0")))
-
-(setf stone-cache (make-hash-table))
-(assert (not (get-from-cache "0")))
-
-
-(defun tree-blink (stones-list times)
-  (if (= 0 times)
-      stones-list
-      (mapcan (lambda (stone)
-                (tree-blink (transform-stone stone) (1- times)))
-          stones-list)))
-
-(defun blink-in-5s (stones-list times)
-  (if (<= times 0)
-      stones-list
-      (apply #'append (mapcar (lambda (stone)
-                (let ((result (or (get-from-cache stone) (tree-blink (split stone) 5))))
-                  (if (not (get-from-cache stone)) (add-to-cache stone result))
-                  (blink-in-5s result (1- times))))
-          stones-list))))
-
-(assert (equal '("1036288" "7" "2" "20" "24" "4048" "1" "4048" "8096" "28" "67" "60" "32") (blink-in-5s (split test-input) 1)))
-
-(assert (string= "253000 1 7" (blink test-input 1)))
-(assert (equal '("253000" "1" "7") (tree-blink (split test-input) 1)))
-(assert (string= "253 0 2024 14168" (blink test-input 2)))
-(assert (string= "1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32" (blink test-input 5)))
-(assert (equal '("1036288" "7" "2" "20" "24" "4048" "1" "4048" "8096" "28" "67" "60" "32") (tree-blink (split test-input) 5)))
-(assert (string= "2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2" (blink test-input 6)))
-(assert (equal '("2097446912" "14168" "4048" "2" "0" "2" "4" "40" "48" "2024" "40" "48" "80" "96" "2" "8" "6" "7" "6" "0" "3" "2") (tree-blink (split test-input) 6)))
-(assert (string= "1 2024 1 0 9 9 2021976" (blink "0 1 10 99 999" 1)))
-
-(defun do-part-1 (input)
-  (length (tree-blink (split input) 25)))
-
-(assert (= 55312 (do-part-1 test-input)))
-(assert (= 55312 (length (tree-blink (split test-input) 25))))
-(assert (= 55312 (length (blink-in-5s (split test-input) 5))))
-
-(defun do-part-2 (input)
-  (length (split (blink input 75))))
+;;********** I need to sort the list and store the amount that things are repeated then dont save the cache, but still dont do duplicate work per level.

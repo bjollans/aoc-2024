200756a117eccde52e3a59247e11fa8302746c3a
Bernard Jollans <berjollans@gmail.com>
2024-12-12 13:35:40 +0100

    Finally ditched stupid cache idea and did day 11 part 2

--- 11.lisp
+++ 11.lisp
@@ -75,21 +75,60 @@
            (reduce-zeroes (subseq stone (/ (length stone) 2)))))
         (t (list (format nil "~d" (* 2024 (parse-integer stone)))))))
 
-(assert (equal (list "1") (transform-stone "0")))
-(assert (equal (list "2024") (transform-stone "1")))
-(assert (equal (list "20" "24") (transform-stone "2024")))
+(defun transform-stones (stones)
+  (mapcan #'transform-stone stones))
 
-(defun tree-blink (stones-list times &optional (depth 0))
-  (if (= depth times)
-      (length stones-list)
-      (apply #'+ (mapcar (lambda (stone)
-                (* (count-val stone stones-list) (tree-blink (transform-stone stone) times (1+ depth))))
-          (unique stones-list)))))
+(assert (equal '("253000" "1" "7") (transform-stones (split test-input))))
 
-(assert (equal 3 (tree-blink (split test-input) 1)))
-(assert (equal 13 (tree-blink (split test-input) 5)))
-(assert (equal 22 (tree-blink (split test-input) 6)))
-(assert (equal 55312 (tree-blink (split test-input) 25)))
+(defun blink (stones times)
+  (if (= 0 times)
+      stones
+      (blink (transform-stones stones) (1- times))))
+
+(assert (equal '("253000" "1" "7") (blink (split test-input) 1)))
+(assert (equal '("253" "0" "2024" "14168") (blink (split test-input) 2)))
+(assert (equal '("2097446912" "14168" "4048" "2" "0" "2" "4" "40" "48" "2024" "40" "48" "80" "96" "2" "8" "6" "7" "6" "0" "3" "2") (blink (split test-input) 6)))
+(assert (equal '("1" "2024" "1" "0" "9" "9" "2021976") (blink (split "0 1 10 99 999") 1)))
+
+(defun cnt-lst-vals (cnt-lst)
+  (unique (mapcar #'second cnt-lst)))
+
+(defun count-cnt-lst-val (cnt-lst val)
+  (apply #'+ (mapcar #'first (remove-if-not (lambda (x) (equal (second x) val)) cnt-lst))))
+
+(assert (equal '(2 3 1) (cnt-lst-vals (list '(1 1) '(3 1) '(1 2) '(10 3) '(12 3) '(1 3) '(1 1)))))
+(assert (equal 23 (count-cnt-lst-val (list '(1 1) '(3 1) '(1 2) '(10 3) '(12 3) '(1 3) '(1 1)) 3)))
+
+(defun simplify-cnt-lst (cnt-lst)
+  (mapcar (lambda (val) (list (count-cnt-lst-val cnt-lst val) val)) (cnt-lst-vals cnt-lst)))
+
+(defun transform-cnt-lst (cnt-lst)
+  (simplify-cnt-lst (mapcan
+      (lambda (x)
+        (let ((new-lst (transform-stone (second x))))
+          (mapcar (lambda (y) (list (first x) y)) new-lst)))
+      cnt-lst)))
+
+(defun get-cnt-lst-len (cnt-lst)
+  (apply #'+ (mapcar #'first cnt-lst)))
+
+(defun transform-lst-rec (cnt-lst times)
+  (if (= 0 times) 
+      cnt-lst
+      (transform-lst-rec (transform-cnt-lst cnt-lst) (1- times))))
+
+(assert (= 55312 (get-cnt-lst-len (transform-lst-rec (list (list 1 "125") (list 1 "17")) 25))))
+
+(defun stones-to-cnt-lst (stones)
+  (simplify-cnt-lst (mapcar (lambda (x) (list 1 x)) (split stones))))
+
+(defun do-part-1/2 (stones times)
+  (get-cnt-lst-len (transform-lst-rec (stones-to-cnt-lst stones) times)))
+
+(assert (= 55312 (do-part-1/2 test-input 25)))
+
+; (defun uniq-cnt-lst (cnt-lst)
+;   )
 
 ; (defun do-part-1 (input)
 ;   (length (tree-blink (split input) 25)))
